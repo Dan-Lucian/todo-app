2ff305db03ae7f82e68e38df0ac2ce10f9f7987e
Dan-Lucian <danugaina1403@gmail.com>
2021-08-24 17:28:19 +0300

    mobile light mode static finished
    Finished mobile static layout, added focus effect on input.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// set handlers for filter buttons
+import { setHandlersForFilterButtons } from './set-handlers-for-filter-buttons.js';
+setHandlersForFilterButtons();
+
+
---
+++ js/set-handlers-for-filter-buttons.js
@@ -0,0 +1,15 @@
+export function setHandlersForFilterButtons() {
+  const tasksFilterList = document.querySelector('.tasks-filter');
+  console.log(tasksFilterList);
+
+  tasksFilterList.onpointerdown = tasksFilter;
+
+  function tasksFilter() {
+    event.preventDefault();
+
+    if (event.target.id === 'tasks-all') console.log('tasks all pressed');
+    if (event.target.id === 'tasks-active') console.log('tasks active pressed');
+    if (event.target.id === 'tasks-completed')
+      console.log('tasks completed pressed');
+  }
+}
